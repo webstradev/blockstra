b9722094c8661067f5c7e36ac12740ca2fbd782c
webstradev <e.s.westra.95@gmail.com>
2023-06-10 16:46:58 +0200

    initial privkey

---
+++ crypto/keys.go
@@ -0,0 +1,49 @@
+package crypto
+
+import (
+	"crypto/ed25519"
+	"crypto/rand"
+	"io"
+)
+
+const (
+	privKeyLen = 64
+	pubKeyLen  = 32
+	seedLen    = 32
+)
+
+func GeneratePrivateKey() *PrivateKey {
+	seed := make([]byte, seedLen)
+	_, err := io.ReadFull(rand.Reader, seed)
+	if err != nil {
+		panic(err)
+	}
+	return &PrivateKey{
+		key: ed25519.NewKeyFromSeed(seed),
+	}
+}
+
+type PrivateKey struct {
+	key ed25519.PrivateKey
+}
+
+func (p *PrivateKey) Bytes() []byte {
+	return p.key
+}
+
+func (p *PrivateKey) Sign(msg []byte) []byte {
+	return ed25519.Sign(p.key, msg)
+}
+
+func (p *PrivateKey) PublicKey() *PublicKey {
+	b := make([]byte, pubKeyLen)
+	copy(b, p.key[32:])
+
+	return &PublicKey{
+		key: b,
+	}
+}
+
+type PublicKey struct {
+	key ed25519.PublicKey
+}
